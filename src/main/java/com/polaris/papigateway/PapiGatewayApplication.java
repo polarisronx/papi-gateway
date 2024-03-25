ac9f7056d00d2049b51c731e32076fa58ee18b8f
Polarisronx <polarisronx@163.com>
2024-03-25 21:02:12 +0800

    【1|初始化】papi-gateway

---
+++ src/main/java/com/polaris/papigateway/PapiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.polaris.papigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapiGatewayApplication {
+
+    public static void main (String[] args){
+        SpringApplication.run(PapiGatewayApplication.class, args);
+    }
+
+}
